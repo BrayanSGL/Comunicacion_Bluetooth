a6cc0385c95c2665bbc7ce3e0361215643e210de
BrayanSGL <bgaleanolara@gmail.com>
2022-07-21 23:41:47 -0500

    Verificacion del la trama lista

---
+++ test.py
@@ -0,0 +1,63 @@
+sequence_example = 'D55S4I10V50D2D5S7I200'
+motor_state = 'stop'
+
+
+def check(sequence):
+    result = True
+    first_test = True
+    second_test = True
+    third_test = True
+
+    # 1ra. letras correctas
+    for i in range(0, len(sequence)):
+        if not sequence[i].isdigit():
+            if sequence[i] == 'D':
+                first_test = True
+            elif sequence[i] == 'I':
+                first_test = True
+            elif sequence[i] == 'V':
+                first_test = True
+            elif sequence[i] == 'S':
+                first_test = True
+            else:
+                first_test = False
+                print('Error #1: "Letra no reconocida"')
+                break
+
+    # 2da. final con letra o inicio con número
+    end_char = sequence[len(sequence)-1:]
+    start_char = sequence[:1]
+    if not end_char.isdigit() or start_char.isdigit():
+        second_test = False
+        print('Error #2: "Secuencia con letra al final o número al principio"')
+
+    # 3ra. comandos juntos o sin número
+    for i in range(0, len(sequence)):
+        if not sequence[i-1].isdigit() and not sequence[i].isdigit():
+            third_test = False
+
+    #Resultado final
+    if not (first_test and second_test and third_test):
+        result = False
+        print('Error #3: "comandos juntos o sin número"')
+    
+    return result
+
+
+def analysis(sequence):
+    for i in range(0, len(sequence)):
+        if not sequence[i].isdigit():
+            letter = sequence[i]
+            new_sequence = sequence[i+1:]
+            for j in range(0, len(new_sequence)):
+                if not new_sequence[j].isdigit():
+                    value = new_sequence[:j]
+                    break
+                if new_sequence.isdigit():
+                    value = int(new_sequence)
+            #Metodo del motor
+            print(letter, value)
+
+
+if check(sequence_example):
+    analysis(sequence_example)
